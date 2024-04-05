5883bc4eb38e41e8de2741b7058b2740ecac4242
Fabiana <fabiana.lua@gmail.com>
2024-04-04 23:26:10 -0300

    Criação do arquivo

---
+++ notifyme-api/src/main/java/notifyme/api/NotifymeApiApplication.java
@@ -0,0 +1,13 @@
+package notifyme.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifymeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifymeApiApplication.class, args);
+	}
+
+}
---
+++ notifyme-api/src/test/java/notifyme/api/NotifymeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package notifyme.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifymeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
