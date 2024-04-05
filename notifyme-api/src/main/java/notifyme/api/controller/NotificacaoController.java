f7e442f7e100b95006031f154d4154c8bfcff50d
Fabiana <fabiana.lua@gmail.com>
2024-04-05 11:39:48 -0300

    NOT-002 Criaçõ do método para atualizar notificação no Service e criação dos endpoints PUT para chamar esse método para atualizar e do DELETE para excluir a notificação

--- notifyme-api/src/main/java/notifyme/api/controller/NotificacaoController.java
+++ notifyme-api/src/main/java/notifyme/api/controller/NotificacaoController.java
@@ -9,9 +9,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -74,6 +76,27 @@ public class NotificacaoController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizarNotificacao(@PathVariable("id") Long id,
+    @RequestBody Map<String, Object> requestBody) {
+        try {
+            notificacaoService.atualizarNotificacao(id, requestBody);
+            return ResponseEntity.status(HttpStatus.OK)
+            .body("Notificação atualizada com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+  
+    
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluirNotificacao(@PathVariable("id") Long id) {
+        notificacaoRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK)
+        .body("Notificação excluída com sucesso!");
     }
     
 }
--- notifyme-api/src/main/java/notifyme/api/service/NotificacaoService.java
+++ notifyme-api/src/main/java/notifyme/api/service/NotificacaoService.java
@@ -3,10 +3,12 @@ package notifyme.api.service;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
 import notifyme.api.constant.Status;
 import notifyme.api.model.Notificacao;
 import notifyme.api.repository.NotificacaoRepository;
@@ -33,6 +35,31 @@ public class NotificacaoService {
         novaNotificacao.setStatus(Status.CRIADA);
         notificacaoRepository.save(novaNotificacao);
 
+    }
+
+    public void atualizarNotificacao(Long id, Map<String, Object> requestBody) {
+        Optional<Notificacao> notifOptional = notificacaoRepository.findById(id);
+
+        if (notifOptional.isPresent()) {
+            Notificacao notifEncontr = notifOptional.get();
+            
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            String dataCriacaoString = (String) requestBody.get("dataCriacao");
+            LocalDateTime dataCriacao = LocalDateTime.parse(dataCriacaoString, formatter);
+
+            String titulo = (String) requestBody.get("titulo");
+            String mensagem = (String) requestBody.get("mensagem");
+
+            notifEncontr.setTitulo(titulo);
+            notifEncontr.setMensagem(mensagem);
+            notifEncontr.setDataCriacao(dataCriacao);
+
+            notificacaoRepository.save(notifEncontr);
+        } else {
+            throw new EntityNotFoundException("Notificação não foi encontrada.");
+        }
+        
+       
     }
     
 }
