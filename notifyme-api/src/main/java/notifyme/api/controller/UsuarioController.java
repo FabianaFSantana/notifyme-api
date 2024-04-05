0f3397340c7c83b322763fe72a8a809b366f9f5c
Fabiana <fabiana.lua@gmail.com>
2024-04-05 00:13:55 -0300

    NOT-001 Criação dos endpoints Get para acessar usuario pelo id e exibir lista de usuarios

--- notifyme-api/src/main/java/notifyme/api/controller/UsuarioController.java
+++ notifyme-api/src/main/java/notifyme/api/controller/UsuarioController.java
@@ -1,8 +1,13 @@
 package notifyme.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +28,19 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED)
         .body(usuarioRepository.save(usuario));
     }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> exbirListaDeUsuarios() {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<Optional<Usuario>> buscarUsuarioPeloId(@PathVariable("idUsuario") Long idUsuario) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(usuarioRepository.findById(idUsuario));
+    }
+
+    
     
 }
