34517b1dba8d8d7194184a9b549000f1443c606d
Fabiana <fabianafernandes.dev@gmail.com>
2024-11-15 04:47:45 -0300

    Adição de dependências do Spring Security e do JWT, modificação do model de Usuario e criação do Model de Endereço

---
+++ src/main/java/notifyme/api/model/Endereco.java
@@ -0,0 +1,22 @@
+package notifyme.api.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+
+public class Endereco {
+    
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
--- src/main/java/notifyme/api/model/Usuario.java
+++ src/main/java/notifyme/api/model/Usuario.java
@@ -1,13 +1,19 @@
 package notifyme.api.model;
 
 
+import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -19,7 +25,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "usuario")
+@Entity(name = "tb_usuarios")
 public class Usuario {
 
     @Id
@@ -29,17 +35,36 @@ public class Usuario {
     @Column(nullable = false)
     private String nome;
 
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
+
+    @Embedded
+    private Endereco endereco;
+
     @NotBlank(message = "O email é obrigatório.")
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória.")
+    @Pattern(regexp = "^(?=.*[A-Z]).{8,}$", message = "A senha deve conter no mínimo 8 caracteres e uma letra maiúscula.")
+    @Column(nullable = false)
+    private String senha;
 
     @NotBlank(message = "O telefone é obrigatório.")
     @Size(min = 10, max = 20, message = "O telefone deve ter 10 a 20 caracteres.")
     @Pattern(regexp = "\\+?[0-9]+", message = "O telefone deve conter apenas números.")
+    @Column(nullable = false)
     private String telefone;
 
-    @OneToMany
+    @OneToMany(cascade = {CascadeType.ALL, CascadeType.REMOVE}, orphanRemoval = true)
+    @JoinColumn(name = "usuario_id")
     private List<Notificacao> notificacoes;
    
+    @Column(nullable = false)
+    private Boolean administrador;
+
+    @Column(nullable = false)
+    private Boolean usuarioExterno;
     
 }
